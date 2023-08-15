7a804adfc4e4c41d80a13814177ad1fd1b4968fc
Misha <term11term@gmail.com>
2023-08-15 17:09:41 +0300

    Конфигурация

--- roadmaps-backend-spring/src/main/java/com/nikitin/roadmaps/roadmapsbackendspring/RoadmapsBackendSpringApplication.java
+++ roadmaps-backend-spring/src/main/java/com/nikitin/roadmaps/roadmapsbackendspring/RoadmapsBackendSpringApplication.java
@@ -2,7 +2,9 @@ package com.nikitin.roadmaps.roadmapsbackendspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RoadmapsBackendSpringApplication {
 
