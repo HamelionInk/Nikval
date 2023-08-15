5f90e73632f99d7c146455c268246209fd4bff8b
Misha <term11term@gmail.com>
2023-08-15 09:09:17 +0300

    Начальная архитектура микросервисов

---
+++ roadmaps-eureka-server/src/test/java/com/example/roadmaps/roadmapseurekaserver/RoadmapsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roadmaps.roadmapseurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadmapsEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ roadmaps-gateway-server/src/test/java/com/nikitin/roadmaps/roadmapsgatewayserver/RoadmapsGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikitin.roadmaps.roadmapsgatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadmapsGatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
