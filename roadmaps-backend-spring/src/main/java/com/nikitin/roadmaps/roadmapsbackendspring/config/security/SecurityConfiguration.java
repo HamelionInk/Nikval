e2da755be56f93dd5481113b71c8d26fdf53ad1d
Misha <term11term@gmail.com>
2023-08-28 13:23:58 +0300

    Конфигурация Spring Security для Resource server

--- roadmaps-backend-spring/src/main/java/com/nikitin/roadmaps/roadmapsbackendspring/config/security/SecurityConfiguration.java
+++ roadmaps-backend-spring/src/main/java/com/nikitin/roadmaps/roadmapsbackendspring/config/security/SecurityConfiguration.java
@@ -1,4 +1,25 @@
 package com.nikitin.roadmaps.roadmapsbackendspring.config.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(httpRequest ->
+                        httpRequest.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2ResourceServer ->
+                        oauth2ResourceServer.jwt(Customizer.withDefaults()));
+
+        return httpSecurity.build();
+    }
+
 }
