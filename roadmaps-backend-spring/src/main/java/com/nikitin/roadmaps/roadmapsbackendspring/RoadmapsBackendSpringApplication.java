ac95a19a38acbaca9363dc7205cdcabd75ea0dbf
Misha <term11term@gmail.com>
2023-09-14 12:28:54 +0300

    CI CD TEST

--- roadmaps-backend-spring/src/main/java/com/nikitin/roadmaps/roadmapsbackendspring/RoadmapsBackendSpringApplication.java
+++ roadmaps-backend-spring/src/main/java/com/nikitin/roadmaps/roadmapsbackendspring/RoadmapsBackendSpringApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class RoadmapsBackendSpringApplication {
 
+    //todo = co cd
     //todo - ci cd
     public static void main(String[] args) {
         SpringApplication.run(RoadmapsBackendSpringApplication.class, args);
