15aa00123b92a4eab941181c1a8c3b447140aa90
Misha <term11term@gmail.com>
2023-09-20 21:38:08 +0300

    Настройка деплоя

--- roadmaps-backend-spring/src/main/java/com/nikitin/roadmaps/roadmapsbackendspring/config/security/SecurityConfiguration.java
+++ roadmaps-backend-spring/src/main/java/com/nikitin/roadmaps/roadmapsbackendspring/config/security/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration {
 
     @Bean
     public JwtDecoder jwtDecoder() {
-        return JwtDecoders.fromOidcIssuerLocation("https://keycloak/realms/roadmaps");
+        return JwtDecoders.fromOidcIssuerLocation("https://keycloak.roadmaps-nikval.ru/realms/roadmaps");
     }
 
     @Bean
