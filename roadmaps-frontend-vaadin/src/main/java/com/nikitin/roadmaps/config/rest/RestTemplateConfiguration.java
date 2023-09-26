754bc2e0e0844a394223baaa5c1816711ebf4ee6
Misha <term11term@gmail.com>
2023-09-26 16:51:43 +0300

    Фикс

--- roadmaps-frontend-vaadin/src/main/java/com/nikitin/roadmaps/config/rest/RestTemplateConfiguration.java
+++ roadmaps-frontend-vaadin/src/main/java/com/nikitin/roadmaps/config/rest/RestTemplateConfiguration.java
@@ -1,5 +1,6 @@
 package com.nikitin.roadmaps.config.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -9,8 +10,8 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 @Configuration
 public class RestTemplateConfiguration {
 
-    //todo - перенести в application
-    private final String url = "https://api.roadmaps-nikval.ru/api/v1/roadmaps";
+    @Value("${backend.url}")
+    private final String url = "http://localhost:8083/api/v1/roadmaps";
 
     @Bean("KeycloakRestTemplate")
     public RestTemplate keycloakRestTemplate() {
