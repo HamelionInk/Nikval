b65896521d6b1ce1c10319d8091799bdd429cc19
msnikitin <nikms1997@yandex.ru>
2024-01-15 04:54:46 +0300

    Настройка CI/CD

--- roadmap-frontend/src/main/java/com/nikitin/roadmapfrontend/component/tree/RoadmapTree.java
+++ roadmap-frontend/src/main/java/com/nikitin/roadmapfrontend/component/tree/RoadmapTree.java
@@ -15,34 +15,4 @@ import java.util.List;
 @Slf4j
 public class RoadmapTree<T extends View> extends Div implements DropTarget<RoadmapTree<T>> {
 
-	private RoadmapData roadmapData;
-	@Getter
-	private List<ChapterTreeItem<T>> items;
-
-	public RoadmapTree(T view, Long roadmapId) {
-		roadmapData = getRoadmapData(view, roadmapId);
-
-		addClassName("roadmap-tree");
-
-		buildTree(view);
-	}
-
-	private RoadmapData getRoadmapData(T view, Long id) {
-		return RoadmapData.builder()
-				.roadmap(view.getClient(RoadmapClient.class).getById(id))
-				.chapters(view.getClient(RoadmapChapterClient.class)
-						.getAll(RoadmapChapterFilter.builder()
-								.roadmapIds(List.of(id))
-								.build())
-						.getRoadmapChapterResponseDtos())
-				.build();
-	}
-
-	private void buildTree(T view) {
-		roadmapData.getChapters().forEach(chapter -> {
-			var item = new ChapterTreeItem<>(view, this, chapter.getId());
-
-			add(item);
-		});
-	}
 }
